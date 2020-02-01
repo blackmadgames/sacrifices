55c0a703da5539d9043a0b78583bf74ac71dc468
Jonathan Landicheff <jlandicheff@kilig.io>
2020-02-01 17:57:18 +0100

    FX Souls turning around player

--- scripts/actors/Player.gd
+++ scripts/actors/Player.gd
@@ -1,9 +1,15 @@
 extends Actor
 
 signal fire
+signal soul_lost
 
 var max_gun_ammo: int = 30
 onready var gun_ammo = max_gun_ammo
+
+var souls_count: int = 10
+
+func _ready() -> void:
+    $SoulsCircle.start(souls_count)
 
 func _process(_delta: float) -> void:
     look_at(get_global_mouse_position())
@@ -25,6 +31,11 @@ func _get_movement_from_input() -> Vector2:
 func _process_attack_input() -> void:
     if Input.is_action_just_pressed("fire"):
         emit_signal("fire", get_parent())
+        spend_soul()
+
+func spend_soul() -> void:
+    souls_count -= 1
+    emit_signal("soul_lost")
 
 func _on_Player_die() -> void:
     print("You're dead'")
