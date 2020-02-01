6d362990463c3e89c8b26e18026489f1c31c1ecb
Jonathan Landicheff <jlandicheff@kilig.io>
2020-02-01 15:11:21 +0100

    Fix asset import alpha

--- scripts/actors/Projectile.gd
+++ scripts/actors/Projectile.gd
@@ -4,4 +4,5 @@ export var lifetime_ms: float = 800
 
 func fire(origin: Vector2, direction: float, force: float) -> void:
     position = origin
+    rotation = direction
     apply_central_impulse((Vector2.UP * force).rotated(direction))
